f3e3612c09ebca2daa4913498316b44a7840e128
Jason Banfelder <jbanfelder@rockefeller.edu>
2022-11-27 16:14:03 -0500

    Include provisioning script in distribution

---
+++ provision.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Provisioning script that gets a new EC2 instance ready to run the s3_archive_manager utility
+
+sudo yum -y update
+sudo yum -y install git
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+git clone https://github.com/banfelder/s3_archive_manager.git
+python3 -m pip install -r s3_archive_manager/requirements.txt
